c3d41ebf0581fed81a3eda880d5c0376c89c4057
haytamelamrani <elamrani.haytam.alae@gmail.com>
2025-05-12 21:12:32 +0100

    reset password

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import NavbarProf from './components/NavbarProf';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import HomePage from './pages/HomePage';
@@ -9,8 +10,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ForgetPassword from './pages/ForgetPassword';
 import ResetPassword from './pages/ResetPassword';
-import UserProfile from './pages/UserProfile'; // ✅ profil utilisateur
-
+import UserProfile from './pages/UserProfile';
 import AboutPage from './pages/AboutPage';
 import FeaturesPage from './pages/FeaturesPage';
 import OtpVerificationPage from './pages/OtpVerificationPage';
@@ -19,6 +19,40 @@ import StudentDashboard from './pages/StudentDashboard';
 import TeacherDashboard from './pages/TeacherDashboard';
 import AdminDashboard from './pages/AdminDashboard';
 import Logout from './pages/Logout';
+
+function AppContent({ darkMode, toggleMode }) {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  return (
+    <>
+      {currentPath.startsWith('/Prof') ? (
+        <NavbarProf darkMode={darkMode} toggleMode={toggleMode} />
+      ) : (
+        <Navbar darkMode={darkMode} toggleMode={toggleMode} />
+      )}
+
+
+      <Routes>
+        <Route path="/" element={<HomePage darkMode={darkMode} toggleMode={toggleMode} />} />
+        <Route path="/login" element={<LoginPage darkMode={darkMode} toggleMode={toggleMode} />} />
+        <Route path="/register" element={<RegisterPage darkMode={darkMode} toggleMode={toggleMode} />} />
+        <Route path="/forgot-password" element={<ForgetPassword darkMode={darkMode} toggleMode={toggleMode} />} />
+        <Route path="/reset-password/:token" element={<ResetPassword darkMode={darkMode} />} />
+        <Route path="/about" element={<AboutPage darkMode={darkMode} toggleMode={toggleMode} />} />
+        <Route path="/features" element={<FeaturesPage darkMode={darkMode} toggleMode={toggleMode} />} />
+        <Route path="/verify-otp" element={<OtpVerificationPage darkMode={darkMode} />} />
+
+        <Route path="/profile" element={<ProtectedRoute><UserProfile darkMode={darkMode} /></ProtectedRoute>} />
+        <Route path="/student-dashboard" element={<ProtectedRoute><StudentDashboard /></ProtectedRoute>} />
+        <Route path="/Prof" element={<ProtectedRoute><TeacherDashboard /></ProtectedRoute>} />
+        <Route path="/admin-dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -29,60 +63,8 @@ function App() {
   }, [darkMode]);
 
   return (
-    
     <Router>
-      <Navbar darkMode={darkMode} toggleMode={toggleMode} />
-
-      <Routes>
-        {/* Pages publiques */}
-        <Route path="/" element={<HomePage darkMode={darkMode} toggleMode={toggleMode} />} />
-        <Route path="/login" element={<LoginPage darkMode={darkMode} toggleMode={toggleMode} />} />
-        <Route path="/register" element={<RegisterPage darkMode={darkMode} toggleMode={toggleMode} />} />
-        <Route path="/forgot-password" element={<ForgetPassword darkMode={darkMode} toggleMode={toggleMode} />} />
-        <Route path="/reset-password/:token" element={<ResetPassword darkMode={darkMode} />} />
-        <Route path="/about" element={<AboutPage darkMode={darkMode} toggleMode={toggleMode} />} />
-        <Route path="/features" element={<FeaturesPage darkMode={darkMode} toggleMode={toggleMode} />} />
-        <Route path="/verify-otp" element={<OtpVerificationPage darkMode={darkMode} />} />
-
-        {/* Page profil utilisateur */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <UserProfile darkMode={darkMode} />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Dashboards sécurisés */}
-        <Route
-          path="/student-dashboard"
-          element={
-            <ProtectedRoute>
-              <StudentDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/teacher-dashboard"
-          element={
-            <ProtectedRoute>
-              <TeacherDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin-dashboard"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Déconnexion */}
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
+      <AppContent darkMode={darkMode} toggleMode={toggleMode} />
     </Router>
   );
 }
