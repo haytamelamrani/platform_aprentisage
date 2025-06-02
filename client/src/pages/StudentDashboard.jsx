16362e0cbe4c66ae69d4f4097507c7b19263360f
haytamelamrani <elamrani.haytam.alae@gmail.com>
2025-06-02 21:21:23 +0100

    ww

--- client/src/pages/StudentDashboard.jsx
+++ client/src/pages/StudentDashboard.jsx
@@ -8,7 +8,6 @@ const StudentDashboard = () => {
        
       <h2>Bienvenue étudiant 👨‍🎓 {userName}</h2>
       <p>Voici ton espace personnel.</p>
-      <button onClick={() => window.location.href = '/logout'} style={{ marginTop: '20px' }}> Déconnexion</button>
      
     </div>
   );
